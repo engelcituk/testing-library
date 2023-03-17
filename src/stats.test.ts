efc75f7cfb26f73618ec93bb62442197104b8441
eCituk <ecituk@thefives.com.mx>
2023-03-17 18:11:39 -0500

    Agrupando los test mediante it y describe

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,35 +1,41 @@
 import {sum, average} from "./stats"
 import * as statsAsync from "./stats"
-import { expect, test} from "./testLib"
+import {describe, expect, it, test} from "./testLib"
 
+describe("stats", () => {
 
-test("calculates the sum of all elements ot the array", ()=>{
-    const result = sum([1,2,3]);
-    const expected = 6;
-  
-    expect(expected).toBe(result);
-});
-  
-test("calculates the average of all elements of the array", ()=>{
-    const result = average([1,2,3]);
-    const expected = 2;
-   
-    expect(expected).toBe(result);
-});
+    it("calculates the sum of all elements ot the array", ()=>{
+        const result = sum([1,2,3]);
+        const expected = 6;
+      
+        expect(expected).toBe(result);
+    });
+      
+    it("calculates the average of all elements of the array", ()=>{
+        const result = average([1,2,3]);
+        const expected = 2;
+       
+        expect(expected).toBe(result);
+    });
+})
 
-test("calculates the sum of all elements ot the array (async)", async ()=>{
-    const result = await statsAsync.sum([1,2,3]);
-    const expected = 6;
-  
-    expect(expected).toBe(result);
-});
-  
-test("calculates the average of all elements of the array (async)", async ()=>{
-    const result = await statsAsync.average([1,2,3]);
-    const expected = 2;
-   
-    expect(expected).toBe(result);
-});
+describe("stats async", () => {
+
+    it("calculates the sum of all elements ot the array (async)", async ()=>{
+        const result = await statsAsync.sum([1,2,3]);
+        const expected = 6;
+      
+        expect(expected).toBe(result);
+    });
+      
+    it("calculates the average of all elements of the array (async)", async ()=>{
+        const result = await statsAsync.average([1,2,3]);
+        const expected = 2;
+       
+        expect(expected).toBe(result);
+    });
+})
+
 
 
 
