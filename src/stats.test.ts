5282f7b222539427b1a8014cc3d16f075b39c1e6
eCituk <ecituk@thefives.com.mx>
2023-03-17 15:16:30 -0500

    Encapsulando los test

---
+++ src/stats.test.ts
@@ -0,0 +1,20 @@
+import {sum, average} from "./stats"
+import { expect, test} from "./testLib"
+
+
+test("calculates the sum of all elements ot the array", ()=>{
+    const result = sum([1,2,3]);
+    const expected = 6;
+  
+    expect(expected).toBe(result);
+});
+  
+test("calculates the average of all elements of the array", ()=>{
+    const result = average([1,2,3]);
+    const expected = 2;
+   
+    expect(expected).toBe(result);
+});
+
+
+  
---
+++ src/testLib.ts
@@ -0,0 +1,19 @@
+export function expect<T>( expected:T ) {
+    return {
+        toBe(result:T){
+            if (result !== expected){
+                throw new Error(`❌ ${result} is not equal to ${expected}`);
+            } 
+        }
+    }
+}
+
+export function test(description: string, callback: () => void ){
+    try {
+        callback();
+        console.log(`✅ ${description}`);
+    } catch (error) {
+        console.log(`❌ ${description}`);
+        console.log(error);        
+    }
+}
