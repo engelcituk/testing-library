d15646c7fb54b1e1b00799b3f298884ed111248c
eCituk <ecituk@thefives.com.mx>
2023-03-17 19:00:57 -0500

    Ignorar y aislar test

--- src/tests/statsAsync.test.ts
+++ src/tests/statsAsync.test.ts
@@ -1,22 +1,6 @@
-import { sum, average} from "./stats"
-import * as statsAsync from "./stats"
 
-describe("stats", () => {
+import * as statsAsync from "./../stats"
 
-    it("calculates the sum of all elements ot the array", ()=>{
-        const result = sum([1,2,3]);
-        const expected = 6;
-      
-        expect(expected).toBe(result);
-    });
-      
-    it("calculates the average of all elements of the array", ()=>{
-        const result = average([1,2,3]);
-        const expected = 2;
-       
-        expect(expected).toBe(result);
-    });
-})
 
 describe("stats async", () => {
 
