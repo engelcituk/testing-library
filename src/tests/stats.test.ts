16b559fb08fba4272c06de9ba227e31f81e5265f
eCituk <ecituk@thefives.com.mx>
2023-03-18 11:15:03 -0500

    modo watch

---
+++ src/tests/stats.test.ts
@@ -0,0 +1,22 @@
+import { sum, average} from "../stats"
+
+describe("stats", () => {
+    it("calculates the sum of all elements ot the array", ()=>{
+        const result = sum([1,2,3]);
+        const expected = 6;      
+        expect(expected).toBe(result);
+    });
+      
+    it("calculates the average of all elements of the array", ()=>{
+        const result = average([1,2,3]);
+        const expected = 2;
+       
+        expect(expected).toBe(result);
+    });
+})
+
+
+
+
+
+  
